93a20da2dc4801937c1de26faa449a8750f0007d
CIT-Vince <vincekimlo.tan@cit.edu>
2024-10-23 22:17:06 +0800

    added customer crud

---
+++ wanderways/src/main/java/com/wanderways/Controller/CustomerController.java
@@ -0,0 +1,51 @@
+package com.wanderways.Controller;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wanderways.Entity.Customer;
+import com.wanderways.Service.CustomerService;
+
+@RestController
+@RequestMapping("/api/customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService custService;
+
+    @GetMapping("/print")
+    public String print() {
+        return "Hello, Customer";
+    }
+
+    @PostMapping("/addCustomer")
+    public Customer addCustomer(@RequestBody Customer customer) {
+        return custService.addCustomer(customer);
+    }
+
+    @GetMapping("/getAllCustomer")
+    public List<Customer> getAllCustomer() {
+        return custService.getAllCustomer();
+    }
+
+    @PutMapping("/updateCustomer/{id}")
+    public Customer updateCustomer(@PathVariable int id, @RequestBody Customer customer) {
+        return custService.updateCustomer(id, customer);
+    }
+
+    @DeleteMapping("/deleteCustomer/{id}")
+    public String deleteCustomer(@PathVariable int id) {
+        return custService.deleteCustomer(id);
+    }
+}
---
+++ wanderways/src/main/java/com/wanderways/Repository/CustomerRepo.java
@@ -0,0 +1,9 @@
+package com.wanderways.Repository;
+
+import com.wanderways.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer, Integer> {
+}
